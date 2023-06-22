41b7f7d503a4860013fd7dc50c4bb51120b9813d
Anand Pandiyan <apandiyan@gmail.com>
2023-06-22 15:44:17 +0400

    Initial Commit

---
+++ textgithub/textgithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  textgithubApp.swift
+//  textgithub
+//
+//  Created by apandiyan on 22/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct textgithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
